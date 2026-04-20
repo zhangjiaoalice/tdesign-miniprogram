23d18e1710a56d4648df1747aa535ae836120b7c
zhangjiaoalice <2640847812@qq.com>
2026-04-20 11:34:50 +0800

    refactor(chat-record): 组件重构

--- packages/pro-components/chat/chat-record/type.ts
+++ packages/pro-components/chat/chat-record/type.ts
@@ -4,52 +4,20 @@
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
  */
 export interface TdChatRecordProps {
-  /**
-   * 是否使用自定义语音输入插槽
-   * @default false
-   */
-  useSpeechInputSlot?: {
+  duration?: {
+    type: NumberConstructor;
+    value: number;
+  };
+  autoSend?: {
     type: BooleanConstructor;
     value: boolean;
   };
-  /**
-   * 是否使用自定义无权限插槽
-   * @default false
-   */
-  useSpeechNoAuthSlot?: {
+  bottomHeight?: {
+    type: NumberConstructor;
+    value: number;
+  };
+  autoHeight?: {
     type: BooleanConstructor;
     value: boolean;
-  };
-  /**
-   * 是否自动抬升发送按钮高度
-   * @default true
-   */
-  autoSendHeight?: {
-    type: BooleanConstructor;
-    value: boolean;
-  };
-}
-
-/** 触摸状态类型 */
-export type TouchStatus = 'top' | 'bottom' | '';
-
-/** 录音状态类型 */
-export type RecordStatus = 'recording' | 'thinking' | 'stop' | 'error' | '';
-
-/** 组件状态类型 */
-export type ComponentStatus = 'normal' | 'cancel' | 'recording' | 'thinking' | 'unknow' | 'complete' | 'error';
-
-/** 触摸事件 */
-export interface CustomTouchEvent {
-  changedTouches?: Array<{
-    clientY: number;
-  }>;
-  preventDefault?: () => void;
-}
-
-/** 焦点事件 */
-export interface CustomFocusEvent {
-  detail?: {
-    height?: number;
   };
 }
